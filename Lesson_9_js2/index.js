21913796e4a97b64bfbd72daa910658a86192265
Tərlan Kərimov <tarlank85@gmail.com>
2024-07-30 11:52:46 +0400

    js_3_1

--- Lesson_9_js2/index.js
+++ Lesson_9_js2/index.js
@@ -268,7 +268,6 @@ console.log(count);
 // 19. Tələbələrin məlumatlarını özündə saxlayan object yaradın. İmtahandan
 // keçməyən tələbələri objectdən silin.
 
-// Создаем объект со студентами
 let students = {
     student1: { name: "Ali", surname: "Aliyev", sum: 55, entrance: 20 },
     student2: { name: "Sara", surname: "Suleymanova", sum: 50, entrance: 16 },
@@ -277,7 +276,6 @@ let students = {
     student5: { name: "Nigar", surname: "Mammadova", sum: 52, entrance: 15 }
 };
 
-// Функция для удаления студентов, не прошедших экзамен
 function removeFailingStudents(students) {
     for (let key in students) {
         if (students[key].sum < 51 || students[key].entrance < 17) {
@@ -286,104 +284,150 @@ function removeFailingStudents(students) {
     }
 }
 
-// Вызов функции для удаления не прошедших студентов
 removeFailingStudents(students);
 
-// Вывод оставшихся студентов
 console.log(students);
 
 //************************************************************************************ */
 // 20. Verilmiş 2mətnin oxşarlıq əmsalını hesablayın. ( 3 ardıcıl sözün eyniliyinə
 // əsasən)
-
+// function getTrigrams(text) {
+//     let words = text.split(/\s+/);
+//     let trigrams = [];
+//     for (let i = 0; i < words.length - 2; i++) {
+//         trigrams.push(words.slice(i, i + 3).join(' '));
+//     }
+//     return trigrams;
+// }
+// let text1="Verilmiş 2 mətnin oxşarlıq əmsalını hesablayın."
+// let text2="mətnin oxşarlıq əmsalını hesablayın. 3 ardıcıl sözün eyniliyinə əsasən"
+// let trig1=getTrigrams(text1)
+// let trig2=getTrigrams(text2)
+// let countTrig=0;
+// for (i=0;i<trig1.length;i++){
+//     for(j=0;j<trig2.length;j++){
+//         if(trig1[i]===trig2[j]) {countTrig++
+        
+//         console.log(trig1[i])
+//         console.log(trig2[j]);}
+//     }
+// }
+// console.log("oxşarlıq əmsalını = ",countTrig);
 //************************************************************************************ */
 // 21. İşçilərin 1 saatlıq əmək haqqı və həftəlik iş saatları verildikdə, aylıq əmək
 // haqqınn hesablanması üçünproqram yazın. Həftəlik 40 saatdan artıq olan iş
 // saatları üçün əmək haqqı 2 qat hesablansın
 
+function calculateMonthlySalary(hourlyWage, weeklyHours) {
+    const regularHours = 40;
+    const overtimeRate = 2;
+    const weeksInMonth = 4;
+
+    let weeklySalary;
+
+    if (weeklyHours <= regularHours) {
+        weeklySalary = weeklyHours * hourlyWage;
+    } else {
+        let regularSalary = regularHours * hourlyWage;
+        let overtimeHours = weeklyHours - regularHours;
+        let overtimeSalary = overtimeHours * hourlyWage * overtimeRate;
+        weeklySalary = regularSalary + overtimeSalary;
+    }
+
+    let monthlySalary = weeklySalary * weeksInMonth;
+    return monthlySalary;
+}
+
+// Примеры использования
+let hourlyWage = 20; // 1 саатlıq əmək haqqı
+let weeklyHours = 45; // Həftəlik iş saatları
+
+let monthlySalary = calculateMonthlySalary(hourlyWage, weeklyHours);
+console.log(`Aylıq əmək haqqı: ${monthlySalary} AZN`); // Output: Aylıq əmək haqqı: 4000 AZN
+
 //************************************************************************************ */
 // 22. Verilmiş mətndə «ADNA» sözlərini «ADNSU» sözü ilə əvəz edin.
-let text22 = "ADNA awfasf dadwada wafawfs awwwa f ADNA";
-console.log(text22.replace("ADNA", "ADNASU"));
+// let text22 = "ADNA awfasf dadwada wafawfs awwwa f ADNA";
+// console.log(text22.replace("ADNA", "ADNASU"));
 //************************************************************************************ */
 // 23. 3 rəqəmli ədədin rəqəmləri cəmini tapın.
-function getSumNum(number) {
-	if (number === 0) {
-		return 0;
-	}
-	return (number % 10) + getSumNum(Math.floor(number / 10));
-}
-let num23 = 3677;
-console.log(getSumNum(num23));
+// function getSumNum(number) {
+// 	if (number === 0) {
+// 		return 0;
+// 	}
+// 	return (number % 10) + getSumNum(Math.floor(number / 10));
+// }
+// let num23 = 3677;
+// console.log(getSumNum(num23));
 
 //************************************************************************************ */
 // 24. Array daxilində tək yerdə duran elementləri ekrana verin
-let array24 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (let i = 0; i < array24.length; i++) {
-	if (i % 2 == 0) console.log(array24[i]);
-}
+// let array24 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < array24.length; i++) {
+// 	if (i % 2 == 0) console.log(array24[i]);
+// }
 //************************************************************************************ */
 // 25. Array elementləri daxilində 5-ə bölünən ədədlərin sayını ekrana verin
 
-let array25 = [1, 2, , 45, 3, 4, 5, 6, 7, 8, 9, 10, 21];
-let newArr = array25.filter((item) => item % 5 == 0);
-console.log("5-ə bölünən ədədlərin sayını = " + newArr.length);
+// let array25 = [1, 2, , 45, 3, 4, 5, 6, 7, 8, 9, 10, 21];
+// let newArr = array25.filter((item) => item % 5 == 0);
+// console.log("5-ə bölünən ədədlərin sayını = " + newArr.length);
 //************************************************************************************ */
 // 26. Array elementləri daxilində 7-ə bölünən sonuncu ədədi ekrana çıxarın.
 
-let newArr26 = array25.filter((item) => item % 7 == 0);
-console.log("7-ə bölünən sonuncu ədəd = " + newArr26[newArr26.length - 1]);
+// let newArr26 = array25.filter((item) => item % 7 == 0);
+// console.log("7-ə bölünən sonuncu ədəd = " + newArr26[newArr26.length - 1]);
 
 //************************************************************************************ */
 // 27. Array elementləri daxilində ən böyük və ən kiçik elementlərin yerini
 // dəyişən proqram tərtib edin
-function swapMinMax(arr) {
-	if (arr27.length < 2) {
-		return arr27; // 2 az olanda yer deyishmenin menasi galmir
-	}
+// function swapMinMax(arr) {
+// 	if (arr27.length < 2) {
+// 		return arr27; // 2 az olanda yer deyishmenin menasi galmir
+// 	}
 
-	let minIndex = 0;
-	let maxIndex = 0;
+// 	let minIndex = 0;
+// 	let maxIndex = 0;
 
-	// min ve max elem index axtarishi
-	for (let i = 1; i < arr27.length; i++) {
-		if (arr27[i] < arr27[minIndex]) {
-			minIndex = i;
-		}
-		if (arr27[i] > arr27[maxIndex]) {
-			maxIndex = i;
-		}
-	}
-	//min ve max yerlerin deyishmeyi
-	let temp = arr27[minIndex];
-	arr27[minIndex] = arr27[maxIndex];
-	arr27[maxIndex] = temp;
-	return arr;
-}
-let arr27 = [3, 1, 4, 1, 5, 9, 2, 6, 5];
-console.log("Original array:", arr27);
+// 	// min ve max elem index axtarishi
+// 	for (let i = 1; i < arr27.length; i++) {
+// 		if (arr27[i] < arr27[minIndex]) {
+// 			minIndex = i;
+// 		}
+// 		if (arr27[i] > arr27[maxIndex]) {
+// 			maxIndex = i;
+// 		}
+// 	}
+// 	//min ve max yerlerin deyishmeyi
+// 	let temp = arr27[minIndex];
+// 	arr27[minIndex] = arr27[maxIndex];
+// 	arr27[maxIndex] = temp;
+// 	return arr;
+// }
+// let arr27 = [3, 1, 4, 1, 5, 9, 2, 6, 5];
+// console.log("Original array:", arr27);
 
-let swappedArray = swapMinMax(arr27.slice());
-console.log("Swapped array:", swappedArray);
+// let swappedArray = swapMinMax(arr27.slice());
+// console.log("Swapped array:", swappedArray);
 
 //************************************************************************************ */
 // 28. Verilmiş array daxilində ixtiyari(random) ədədi ekrana çıxaran proqram
 // tərtib edin
-function getRandomElement(arr) {
-	// Генерируем случайный индекс от 0 до длины массива - 1
-	let randomIndex = Math.floor(Math.random() * arr.length);
-	return arr[randomIndex];
-}
+// function getRandomElement(arr) {
+// 	// Генерируем случайный индекс от 0 до длины массива - 1
+// 	let randomIndex = Math.floor(Math.random() * arr.length);
+// 	return arr[randomIndex];
+// }
 
-let randomElement = getRandomElement(arr27);
-console.log("Random element : ", randomElement);
+// let randomElement = getRandomElement(arr27);
+// console.log("Random element : ", randomElement);
 
 //************************************************************************************ */
 // 29. 1-dən 100-ə qədər ədədlər daxilində təkliyi 1 olan ədədləri ekrana verin
 
-for (i = 1; i <= 100; i++) {
-	if (i % 10 == 1) console.log("təkliyi 1 olan ədədlər : ", i);
-}
+// for (i = 1; i <= 100; i++) {
+// 	if (i % 10 == 1) console.log("təkliyi 1 olan ədədlər : ", i);
+// }
 //************************************************************************************ */
 // 30. Daxil olunmuş ifadənin polindrom olduğunu yoxlayın. (polindrom
 // dedikdə tərsi və düzü eyni olan ifadə başa düşülür məsələn ütü, 121,1331,4554
@@ -501,19 +545,19 @@ for (i = 1; i <= 100; i++) {
 //************************************************************************************ */
 // 36. Daxil olunan ixtiyari ədədin rəqəmləri cəmini tapan funksiya yaradın1.
 
-function Sum36(num) {
-	if (num == 0) return 0;
-	return (num % 10) + Sum36(Math.floor(num / 10));
-}
-function isNumber36(num) {
-	return typeof num === "number" && !isNaN(num);
-}
+// function Sum36(num) {
+// 	if (num == 0) return 0;
+// 	return (num % 10) + Sum36(Math.floor(num / 10));
+// }
+// function isNumber36(num) {
+// 	return typeof num === "number" && !isNaN(num);
+// }
 
-let number36 = +prompt("Eded daxil edin");
-// let number36=9989
-console.log(number36);
-if (isNumber36(number36)) {
-	console.log(Sum36(number36));
-} else {
-	console.log("Daxil etdyiniz edet deyil");
-}
+// let number36 = +prompt("Eded daxil edin");
+// // let number36=9989
+// console.log(number36);
+// if (isNumber36(number36)) {
+// 	console.log(Sum36(number36));
+// } else {
+// 	console.log("Daxil etdyiniz edet deyil");
+// }
